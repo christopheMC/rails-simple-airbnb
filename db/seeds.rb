3e7684ab56d73066f9b0175372c325d145d624cd
christophe mohammed-cherif <krisfromtama@gmail.com>
2020-08-12 22:10:23 +0200

    airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Beautiful place in Paris',
+  address: 'Rue des Champs Elysées',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 150,
+  number_of_guests: 2
+)
+Flat.create!(
+  name: 'Grand appartement à Marseille',
+  address: 'Vieux Port',
+  description: 'Proche du terrain de boules',
+  price_per_night: 50,
+  number_of_guests: 10
+)
+Flat.create!(
+  name: 'Joli coin de paradis à Tahiti',
+  address: 'Place du lagon',
+  description: 'Paradis sur Terre',
+  price_per_night: 200,
+  number_of_guests: 2
+)
+
